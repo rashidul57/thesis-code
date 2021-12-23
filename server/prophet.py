b12220dcc5043374250b0f10fbe65761bedfa6cc
Rashidul Islam <mrashidbd2000@gmail.com>
2021-12-23 00:14:25 -0400

    implemented drilled models with texture

--- server/prophet.py
+++ server/prophet.py
@@ -1,33 +1,58 @@
-# https://machinelearningmastery.com/time-series-forecasting-with-prophet-in-python/
+# # https://machinelearningmastery.com/time-series-forecasting-with-prophet-in-python/
 
-# make an out-of-sample forecast
-from pandas import read_csv
-from pandas import to_datetime
-from pandas import DataFrame
-from fbprophet import Prophet
-from matplotlib import pyplot
-# load data
-path = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-car-sales.csv'
-df = read_csv(path, header=0)
-# prepare expected column names
-df.columns = ['ds', 'y']
-df['ds']= to_datetime(df['ds'])
-# define the model
-model = Prophet()
-# fit the model
-model.fit(df)
-# define the period for which we want a prediction
-future = list()
-for i in range(1, 13):
-	date = '1969-%02d' % i
-	future.append([date])
-future = DataFrame(future)
-future.columns = ['ds']
-future['ds']= to_datetime(future['ds'])
-# use the model to make a forecast
-forecast = model.predict(future)
-# summarize the forecast
-print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
-# plot forecast
-model.plot(forecast)
-pyplot.show()
+# # make an out-of-sample forecast
+# from pandas import read_csv
+# from pandas import to_datetime
+# from pandas import DataFrame
+# from fbprophet import Prophet
+# from matplotlib import pyplot
+# # load data
+# path = 'https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-car-sales.csv'
+# df = read_csv(path, header=0)
+# # prepare expected column names
+# df.columns = ['ds', 'y']
+# df['ds']= to_datetime(df['ds'])
+# # define the model
+# model = Prophet()
+# # fit the model
+# model.fit(df)
+# # define the period for which we want a prediction
+# future = list()
+# for i in range(1, 13):
+# 	date = '1969-%02d' % i
+# 	future.append([date])
+# future = DataFrame(future)
+# future.columns = ['ds']
+# future['ds']= to_datetime(future['ds'])
+# # use the model to make a forecast
+# forecast = model.predict(future)
+# # summarize the forecast
+# print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].head())
+# # plot forecast
+# model.plot(forecast)
+# # pyplot.show()
+
+
+
+# https://facebook.github.io/prophet/docs/quick_start.html#python-api
+
+import pandas as pd
+from prophet import Prophet
+
+df = pd.read_csv('server/data/phophet.csv')
+df.head()
+
+m = Prophet()
+m.fit(df)
+
+future = m.make_future_dataframe(periods=365)
+future.tail()
+
+forecast = m.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+
+fig1 = m.plot(forecast)
+
+
+
