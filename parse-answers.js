00eb0667523a8a60cdb5125c63410a2cac649100
rashidul57 <mrashidbd2000@gmail.com>
2022-03-24 12:12:06 -0300

    updates

--- parse-answers.js
+++ parse-answers.js
@@ -6,20 +6,18 @@ const directoryPath = path.join(__dirname, 'docs/survey/answers');
 //passsing directoryPath and callback function
 const files = fs.readdirSync(directoryPath).filter(file => file !== '.DS_Store' 
   && file !== 'sbrooks@cs.dal.ca.json')
-  //listing all files using forEach
+
 const modules = ['ca-bubble', 'ca-grid', 'vsup-bubble', 'vsup-grid'];
 const totals = {}
-const eights = {}
-files.forEach((file, indx) => {
-    // console.log(file); 
-    const data = fs.readFileSync(directoryPath + '/' + file, 'utf8');
-    
-    const answers = JSON.parse(data);
 
-    console.log(file, ' - ', answers['participant-num'])
-    let result = [];
+let studyResults = [];
+let susResults = []
+let nasaTlxResults = []
+files.forEach((file, indx) => {
+    const data = fs.readFileSync(directoryPath + '/' + file, 'utf8');
+    const answers = JSON.parse(data);
+    const result = [];
     modules.forEach(prop => {
-      // console.log(prop, ': ' , answers[prop])
       let correctCount = 0;
       for (let indx in answers[prop]) {
         if (answers[prop][indx]) {
@@ -27,17 +25,93 @@ files.forEach((file, indx) => {
         }
       }
       totals[prop] = (totals[prop] || 0) + correctCount;
-      if (correctCount === 8) {
-        eights[prop] = (eights[prop] || 0) + 1
-      }
-      result.push(prop + ': ' + correctCount);
+      result.push(correctCount);
     })
-    console.log(result);
-    // console.log(answers)
+    studyResults.push(result)
+
+    // nasa-tlx data
+    const nasaRow = []
+    for (let prop in answers['nasa-tlx']) {
+      nasaRow.push(answers['nasa-tlx'][prop])
+    }
+    nasaTlxResults.push(nasaRow)
+
+    // sus data
+    const susRow = []
+    for (let prop in answers['sus']) {
+      susRow.push(answers['sus'][prop])
+    }
+    susResults.push(susRow)
 });
 
+const result = [];
 modules.forEach(prop => {
-  console.log(prop + ': ' + totals[prop]/files.length, eights[prop])
+  const avg = totals[prop]/files.length;
+  result.push(Number(avg.toFixed(1)));
 });
 
-console.log(files.length)
+studyResults.push(result)
+
+writeStudyResults(modules, studyResults)
+writeSusResults(susResults, 'sus.csv')
+writeSusResults(nasaTlxResults, 'nasa-tlx.csv')
+// console.log(susResults[0])
+
+
+function writeSusResults(results, fileName) {
+  const header = [{id: 'participant', title: 'Participant #'}]
+  results[0].forEach((val, indx) => {
+    header.push({id: indx, title: 'Q#' + (indx + 1)})
+  })
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const csvWriter = createCsvWriter({
+    path: './docs/survey/calc-results/' + fileName,
+    header
+  });
+
+  const data = results.map((result, ind) => {
+    const row = {participant: (ind+1)}
+    result.forEach((value, indx) => {
+      row[indx] = value;
+    })
+    return row;
+  })
+  const avg = {}
+  for (let prop in data[0]) {
+    let sum = 0;
+    data.forEach(row => {
+      sum += row[prop]
+    })
+    avg[prop] = Number((sum/data.length).toFixed(1))
+  }
+  avg['participant'] = 'Avg'
+  // console.log(avg)
+  data.push(avg)
+
+  csvWriter
+    .writeRecords(data)
+    .then(()=> console.log('Results written'));
+}
+
+function writeStudyResults(modules, results) {
+  const header = [{id: 'participant', title: 'Participant #'}]
+  modules.forEach((prop) => {
+    header.push({id: prop, title: prop})
+  })
+  const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+  const csvWriter = createCsvWriter({
+    path: './docs/survey/calc-results/study.csv',
+    header
+  });
+  const data = results.map((result,ind) => {
+    const row = {participant: (ind+1)}
+    modules.forEach((prop, indx) => {
+      row[prop] = result[indx];
+    })
+    return row;
+  })
+
+  csvWriter
+    .writeRecords(data)
+    .then(()=> console.log('Study Results written'));
+}
